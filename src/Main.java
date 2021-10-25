0a059d14c91c5f11d83388f3d0340fecaa691984
CalebPerry <caleb.perry@cecstudents.org>
2021-10-25 08:45:47 -0600

    done need comments

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,30 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> pass = new ArrayList<>();
-        pass.add(1);
-        pass.add(3);
-        pass.add(2);
-        pass.add(4);
-        pass.add(3);
-        pass.add(5);
-        System.out.println(Search.removeDuplicates(pass));
+        ArrayList<Integer> passInteger = new ArrayList<>();
+        passInteger.add(1);
+        passInteger.add(3);
+        passInteger.add(2);
+        passInteger.add(4);
+        passInteger.add(3);
+        passInteger.add(5);
+        ArrayList<Character> passCharacter = new ArrayList<>();
+        passCharacter.add('a');
+        passCharacter.add('c');
+        passCharacter.add('b');
+        passCharacter.add('d');
+        passCharacter.add('c');
+        passCharacter.add('e');
+        ArrayList<String> passString = new ArrayList<>();
+        passString.add("apple");
+        passString.add("cashew");
+        passString.add("banana");
+        passString.add("dates");
+        passString.add("cashew");
+        passString.add("emu-berry");
+
+        System.out.println(Search.removeDuplicates(passInteger));
+        System.out.println(Search.removeDuplicates(passCharacter));
+        System.out.println(Search.removeDuplicates(passString));
     }
 }
--- src/Search.java
+++ src/Search.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Search{
     public static <E>ArrayList<E> removeDuplicates(ArrayList<E> list){
-        for(E i : list){
-
+        for(int i = 0; i < list.size()-1; i++){
+            for(int j = 0; j < list.size()-1; j++){
+                if(Objects.equals(list.get(i).toString(), list.get(j).toString()) && j != i)list.remove(j);
+            }
         }
         return list;
     }
